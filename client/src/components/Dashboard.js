5874ea1ddc9cbeba90d1b5536cb76fcd01cf10cd
duonghailee <duonghailee@gmail.com>
2018-05-19 14:02:56 +0300

    Form should be displayed only when user click + add button

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,15 +1,14 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import axios from "axios";
-
+import TaskForm from "./TaskForm";
 
 class Dashboard extends Component {
   constructor() {
     super();
     this.state = {
       rows: [],
-      isLoading: false,
-      showForm : false,
+      showForm: false,
     };
     this.getAllTasks = this.fetchAllTasks.bind(this);
     this.clearAll = this.clearAllTasks.bind(this);
@@ -17,118 +16,60 @@ class Dashboard extends Component {
   }
 
   fetchAllTasks() {
-    axios.get('/api/getTasks').then(response => {this.setState({ rows: response.data, isLoading: true }) ; console.log(response)}).catch(error => console.log(error))
+    axios.get('/api/getTasks').then(response => { this.setState({ rows: response.data }); console.log(response) }).catch(error => console.log(error))
   }
 
   clearAllTasks() {
     const id = '5ae4eb09dbaac609b7379a45';
-    axios.post('/api/completeTask', {taskid: id}).then(response => console.log(response)).catch(error => console.log(error))
+    axios.post('/api/completeTask', { taskid: id }).then(response => console.log(response)).catch(error => console.log(error));
+    this.setState({ rows: [] })
   }
 
-  addNewTask(){
-   /* this.setState({ {this.state.showForm ? false : true }}) */
-   this.state.showForm = true;
+  addNewTask() {
+    /* this.setState({ {this.state.showForm ? false : true }}) */
+    this.setState({ showForm: true });
   }
 
 
 
   render() {
+
+    /*
     const isLoading = this.state.isLoading;
 
     const loading_icon = isLoading ? (<div><i className="fa fa-spinner fa-pulse fa-3x fa-fw"></i>
       <span className="sr-only">Loading...</span></div>) : (<div></div>);
-    const clear_all_button = isLoading ? (<button className="btn-floating pulse right" onClick={this.clearAll} type="button"><i className="small material-icons">clear_all</i></button>) : (<div></div>);
+    */
+    const clear_all_button = (<button className="btn-floating pulse right" onClick={this.clearAll} type="button"><i className="small material-icons">clear_all</i></button>);
+    const add_button = (<a href="#task_form" class="btn-floating btn-medium waves-effect waves-light red right" onClick={this.addTask} ><i class="material-icons">add</i></a>)
 
     return (
       <div>
 
         <div className="text-center">
+          {/* Fix me, I know it's not easy, somehow getting user name here */}
           <h3>Dashboard, welcome duonghailee </h3>
         </div>
         <div>
           <button class="btn waves-effect waves-light" type="button" onClick={this.getAllTasks}>Your tasks</button>
-          <a class="btn-floating btn-medium waves-effect waves-light red right" onClick={this.addTask}><i class="material-icons">add</i></a>
+          {add_button}
         </div>
         {/*{loading_icon}*/}
+
         <div id="totolist">
           <ul className="collection">
-            {this.state.rows.map((row, index) => <li key={row.title + index} className="collection-item"><div>{row.title}<a href="#" className="secondary-content"><i className="material-icons">send</i></a></div></li>)}
+            {this.state.rows.map((row, index) => <li key={row.title + index} className="collection-item"><div>{row.title}<a href="#" className="secondary-content"><i className="material-icons">check</i></a></div></li>)}
           </ul>
           {clear_all_button}
-         
+
         </div>
 
+        {this.state.showForm ? <TaskForm refresh={this.fetchAllTasks} /> : <div></div>}
 
-
-        <TaskForm showForm={this.state.showForm}/>
       </div>
     )
   }
 };
-
-/* create a form to insert new task */
-class TaskForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { title: '', description: '', start: '', end: '', complete: false }
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({ [event.target.name]: event.target.value });
-  }
-
-  handleSubmit(event) {
-    console.log(this.state)
-    event.preventDefault();
-    axios.post('/api/addTask', {
-      title: this.state.title,
-      description: this.state.description,
-      start: this.state.start,
-      end: this.state.end
-    }).then(function (response) {
-      console.log(response);
-    }).catch(function (error) {
-      console.log(error);
-    })
-  }
-
-  render() {
-    const task_form = this.props.showForm ? (<form onSubmit={this.handleSubmit} >
-      <label>
-        Title:
-        <input type="text" name='title' required onChange={this.handleChange} />
-      </label>
-
-      <label>
-        Description:
-        <input type="text" name='description' required onChange={this.handleChange} />
-      </label>
-
-      <label>
-        Start:
-        <input type="date" name='start' required onChange={this.handleChange} />
-      </label>
-
-      <label>
-        End:
-        <input type="date" name='end' required onChange={this.handleChange} />
-      </label>
-
-      <input type='submit' value="Submit" />
-    </form >) : (<div></div>)
-    return (
-      <div>
-        {task_form}
-      </div>
-
-    )
-  }
-
-
-}
 
 
 export default Dashboard;
