d1ba56347a999b9df8f432f652728e6b85bb0b4a
duonghailee <duonghailee@gmail.com>
2018-05-19 19:08:59 +0300

    Submit a form also update and render the task list automatically

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import axios from "axios";
 import TaskForm from "./TaskForm";
 
+
 class Dashboard extends Component {
   constructor() {
     super();
@@ -10,12 +11,12 @@ class Dashboard extends Component {
       rows: [],
       showForm: false,
     };
-    this.getAllTasks = this.fetchAllTasks.bind(this);
+    this.getAllTasks = this.getAllTasks.bind(this);
     this.clearAll = this.clearAllTasks.bind(this);
     this.addTask = this.addNewTask.bind(this);
   }
 
-  fetchAllTasks() {
+  getAllTasks() {
     axios.get('/api/getTasks').then(response => { this.setState({ rows: response.data }); console.log(response) }).catch(error => console.log(error))
   }
 
@@ -41,7 +42,20 @@ class Dashboard extends Component {
       <span className="sr-only">Loading...</span></div>) : (<div></div>);
     */
     const clear_all_button = (<button className="btn-floating pulse right" onClick={this.clearAll} type="button"><i className="small material-icons">clear_all</i></button>);
-    const add_button = (<a href="#task_form" class="btn-floating btn-medium waves-effect waves-light red right" onClick={this.addTask} ><i class="material-icons">add</i></a>)
+    const add_button = (<a href="#task_form" className="btn-floating btn-medium waves-effect waves-light red right" onClick={this.addTask} ><i className="material-icons">add</i></a>)
+    const hidden_info = (<div><p>
+      <a className="btn btn-primary" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+        Link with href
+      </a>
+      <button className="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+        Button with data-target
+      </button>
+    </p>
+      <div className="collapse" id="collapseExample">
+        <div className="card card-body">
+          Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+      </div>
+      </div></div>)
 
     return (
       <div>
@@ -51,20 +65,20 @@ class Dashboard extends Component {
           <h3>Dashboard, welcome duonghailee </h3>
         </div>
         <div>
-          <button class="btn waves-effect waves-light" type="button" onClick={this.getAllTasks}>Your tasks</button>
+          <button id="view_all_task" className="btn waves-effect waves-light" type="button" onClick={this.getAllTasks}>Your tasks</button>
           {add_button}
         </div>
         {/*{loading_icon}*/}
 
-        <div id="totolist">
+        <div id="task-list">
           <ul className="collection">
-            {this.state.rows.map((row, index) => <li key={row.title + index} className="collection-item"><div>{row.title}<a href="#" className="secondary-content"><i className="material-icons">check</i></a></div></li>)}
+            {this.state.rows.map((row, index) => <li key={row.title + index} className="collection-item"><div>{row.title}<a className="secondary-content"><i className="material-icons">arrow_drop_down</i></a></div></li>)}
           </ul>
           {clear_all_button}
 
         </div>
-
-        {this.state.showForm ? <TaskForm refresh={this.fetchAllTasks} /> : <div></div>}
+        { /*{hidden_info} */}
+        {this.state.showForm ? <TaskForm refresh={this.getAllTasks} /> : <div></div>}
 
       </div>
     )
