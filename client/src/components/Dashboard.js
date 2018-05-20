23f12d042031ae3798f55ab66af21dbff4364a86
duonghailee <duonghailee@gmail.com>
2018-05-20 11:06:51 +0300

    Dropdown button should show task detail, test only

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import axios from "axios";
 import TaskForm from "./TaskForm";
+import Task from "./Task"
 
 
 class Dashboard extends Component {
@@ -10,14 +11,16 @@ class Dashboard extends Component {
     this.state = {
       rows: [],
       showForm: false,
+      showTaskDetail: false,
     };
     this.getAllTasks = this.getAllTasks.bind(this);
-    this.clearAll = this.clearAllTasks.bind(this);
-    this.addTask = this.addNewTask.bind(this);
+    this.clearAllTasks = this.clearAllTasks.bind(this);
+    this.addNewTask = this.addNewTask.bind(this);
+    this.handleSingleTask = this.handleSingleTask.bind(this);
   }
 
   getAllTasks() {
-    axios.get('/api/getTasks').then(response => { this.setState({ rows: response.data }); console.log(response) }).catch(error => console.log(error))
+    axios.get('/api/getTasks').then(response => { this.setState({ rows: response.data }); console.log(response) }).catch(error => alert('please login'))
   }
 
   clearAllTasks() {
@@ -31,6 +34,10 @@ class Dashboard extends Component {
     this.setState({ showForm: true });
   }
 
+  handleSingleTask(){
+    this.setState({showTaskDetail: this.state.showTaskDetail ? false : true})
+  }
+
 
 
   render() {
@@ -41,28 +48,16 @@ class Dashboard extends Component {
     const loading_icon = isLoading ? (<div><i className="fa fa-spinner fa-pulse fa-3x fa-fw"></i>
       <span className="sr-only">Loading...</span></div>) : (<div></div>);
     */
-    const clear_all_button = (<button className="btn-floating pulse right" onClick={this.clearAll} type="button"><i className="small material-icons">clear_all</i></button>);
-    const add_button = (<a href="#task_form" className="btn-floating btn-medium waves-effect waves-light red right" onClick={this.addTask} ><i className="material-icons">add</i></a>)
-    const hidden_info = (<div><p>
-      <a className="btn btn-primary" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
-        Link with href
-      </a>
-      <button className="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
-        Button with data-target
-      </button>
-    </p>
-      <div className="collapse" id="collapseExample">
-        <div className="card card-body">
-          Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
-      </div>
-      </div></div>)
+    const clear_all_button = (<button className="btn-floating pulse right" onClick={this.clearAllTasks} type="button"><i className="small material-icons">clear_all</i></button>);
+    const add_button = (<a href="#task_form" className="btn-floating btn-medium waves-effect waves-light red right" onClick={this.addNewTask} ><i className="material-icons">add</i></a>)
+    const task_detail = this.state.showTaskDetail ? (<p>show task </p>) : <div></div>
 
     return (
       <div>
 
         <div className="text-center">
           {/* Fix me, I know it's not easy, somehow getting user name here */}
-          <h3>Dashboard, welcome duonghailee </h3>
+          <h4>Dashboard, How do you do </h4>
         </div>
         <div>
           <button id="view_all_task" className="btn waves-effect waves-light" type="button" onClick={this.getAllTasks}>Your tasks</button>
@@ -72,7 +67,7 @@ class Dashboard extends Component {
 
         <div id="task-list">
           <ul className="collection">
-            {this.state.rows.map((row, index) => <li key={row.title + index} className="collection-item"><div>{row.title}<a className="secondary-content"><i className="material-icons">arrow_drop_down</i></a></div></li>)}
+            {this.state.rows.map((row, index) => <li key={row.title + index} className="collection-item"><div>{row.title}<a onClick={this.handleSingleTask} role="button" className="secondary-content"><i className="material-icons">arrow_drop_down</i></a>{task_detail}</div></li>)}
           </ul>
           {clear_all_button}
 
